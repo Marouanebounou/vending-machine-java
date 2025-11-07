4cc3e952ab0fe791449092beb0c75bf39cdd548a
mehdi el-hajjame <el-hajjame@outlook.com>
2025-11-07 15:11:29 +0100

    add dispaly menu and show product list

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,15 +1,66 @@
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class VendingMachine {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class VendingMachine {
+    public static void mainMenu() {
+
+        while (true) {
+            System.out.println("===========================================");
+            System.out.println("DISTRIBUTEUR AUTOMATIQUE");
+            System.out.println("===========================================");
+            System.out.println("[1] Afficher les produits");
+            System.out.println("[2] Acheter un produit");
+            System.out.println("[3] Quitter");
+            System.out.println("===========================================");
+
+            Scanner input = new Scanner(System.in);
+            System.out.println("Entrez votre choix : ");
+            int choix = input.nextInt();
+
+            if (choix == 3) {
+                System.out.println("Merci d’avoir utilisé notre distributeur !");
+                break;
+            }
+
+            switch (choix) {
+                case 1:
+                    display();
+                    break;
+
+                case 2:
+                    acheterProduit();
+                    break;
+
+            }
         }
+    }
+
+    public void pruchadeMenu() {
+        while (true) {
+
+        }
+    }
+
+    public static void display() {
+
+        ArrayList<String> produits = new ArrayList<>(Arrays.asList("Eau", "Soda", "Chips", "Chocolat"));
+
+        ArrayList<Double> prix = new ArrayList<>(Arrays.asList(5.0, 8.0, 12.0, 15.0));
+
+        ArrayList<Integer> stock = new ArrayList<>(Arrays.asList(10, 5, 7, 3));
+
+        // display
+
+        for (int i = 0; i < produits.size(); i++) {
+            System.out.println(String.format("%-15s | Prix: %-6s DH | Stock: %d",
+                    produits.get(i), prix.get(i), stock.get(i)));
+        }
+    }
+
+    public static void main(String[] args) {
+        mainMenu();
+
     }
 }
